6065301531a305734e56c4528334439bcd69ba22
abo3adel <abo3adel35@gmail.com>
2020-08-02 21:02:52 +0100

    show icon next to website name

--- resources/views/layouts/nav/index.blade.php
+++ resources/views/layouts/nav/index.blade.php
@@ -1,26 +1,32 @@
- @include('layouts.nav.top_nav')
+@include('layouts.nav.top_nav')
 {{-- <div class="row mr-0">
     <div class="col-12 text-center bg-dark pb-1">
-        <a href="/{{app()->getLocale()}}">
-            <img class="rounded mx-auto d-block" width="350" src="/storage/site/logo-big.png" />
-        </a>
-    </div>
+        <a href="/{{ app()->getLocale() }}">
+<img class="rounded mx-auto d-block" width="350" src="/storage/site/logo-big.png" />
+</a>
+</div>
 </div> --}}
 {{-- :style="(h.d.carouselHeight && h.d.scrollTop > (h.d.carouselHeight -20)) ? 'background-color: var(--blue) !important;background: linear-gradient(120deg, #101a50, transparent);' : ''" --}}
-<nav id='index-nav' class="navbar sticky-top navbar-expand navbar-dark bg-primary shadow-sm transition" :class="{'bg-transparent': (h.d.carouselHeight && h.d.scrollTop < (h.d.carouselHeight -20))}">
+<nav id='index-nav' class="navbar sticky-top navbar-expand navbar-dark bg-primary shadow-sm transition"
+    :class="{'bg-transparent': (h.d.carouselHeight && h.d.scrollTop < (h.d.carouselHeight -20))}">
     <div class="container-fluid">
         <x-a class="navbar-brand ml-3" href="/">
-            {{-- <img class="rounded mx-auto d-block" width="70" src="/storage/site/logo-big.png" /> --}}
-            <strong class="text-center text-warning">Dress</strong>
-            <small class="smallDress">
-                {{__('nav.dressMess')}}
-            </small>
+            <div class="row p-0 m-0">
+                <div class="col-3 p-0 m-0">
+                    <img class="rounded mx-auto" width="20" src="/storage/site/logo-big.png" />
+                </div>
+                <div class="col-9 p-0 m-0">
+                    <strong class="text-center text-warning">Dress</strong>
+                    <small class="smallDress">
+                        {{ __('nav.dressMess') }}
+                    </small>
+                </div>
+            </div>
         </x-a>
-        <button type="button"
-            class="navbar-brand navbar-toggler d-block d-md-none"
+
+        <button type="button" class="navbar-brand navbar-toggler d-block d-md-none"
             v-on:click.prevent.stop="h.d.openSide('menuSidebar')">
-            <span class="navbar-toggler-icon"
-                aria-label="{{ __('Toggle_navigation') }}"></span>
+            <span class="navbar-toggler-icon" aria-label="{{ __('Toggle_navigation') }}"></span>
         </button>
 
         <div class=" navbar-collapse" id="navbarSupportedContent">
@@ -40,6 +46,6 @@
         </div>
     </div>
 </nav>
-@isset ($cats)
-@include('layouts.nav.bottom_nav')
+@isset($cats)
+    @include('layouts.nav.bottom_nav')
 @endisset
