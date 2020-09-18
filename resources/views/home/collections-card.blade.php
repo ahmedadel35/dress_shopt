e694053460082b3c139bdd572b746680097dd667
abo3adel <abo3adel35@gmail.com>
2020-09-18 07:05:43 +0100

    fix categories cards height

--- resources/views/home/collections-card.blade.php
+++ resources/views/home/collections-card.blade.php
@@ -1,5 +1,5 @@
 @foreach ($cats as $c)
-<div class="collection-card col-6 col-sm-4 col-md-3 px-1">
+<div class="collection-card col-sm-4 col-md-3 px-1 cat-card">
     <div class="card p-0 text-white overflow-hidden" v-lazy-container="{ selector: 'img' }">
         <img data-src="/storage/collection/{{$c->id}}.jpg"
             class="card-img transition" alt="{{$c->title}}">
